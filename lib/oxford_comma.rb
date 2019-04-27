93da5a6d07646363f8ffe5c11c5478d2bd95c676
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-27 18:51:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
 
 def oxford_comma(array)
-
+  if array.first
   elsif array.join(" and ")
   elsif array[0..2]
     end 
