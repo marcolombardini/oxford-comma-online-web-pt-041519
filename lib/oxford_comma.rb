ba9f3d21b1534808c50cd2bd45c0f93d471406ee
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-27 20:42:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2 
   array.join(" and ")
-  elsif array.length
-  array = array[-1].insert(0, "and ")
+  elsif array.join(", ")
+  array[-1].insert(0, "and ")
   end
 end
