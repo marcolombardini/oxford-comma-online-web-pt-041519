5f36395d7e5325508f6198c97647e0830c2fac7c
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-27 18:42:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
-def oxford_comma(array)
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
 
+def oxford_comma(array)
+    array.first
+    array.join(" and ")
+    if arrays = array
+      
 end
