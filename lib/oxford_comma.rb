17891684b0a4e6bc11622749cf292c1909f03654
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-27 20:11:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.first
   elsif array.length == 2 
   array.join(" and ")
-  elsif array.length >= 3
+  elsif array.length == 3
   array[0...3].join(", ")
   array.last(" and ")
   end
